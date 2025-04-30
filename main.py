07bd8a37c0a6f91eb027afd2e4a14836ee7c0104
s2h25 <keithenvalentine@unomaha.edu>
2025-04-30 03:55:59 +0000

    keiley kylei

--- main.py
+++ main.py
@@ -33,19 +33,19 @@ def main():
   print("Begin Sorting %d elements." % numberTerms)
 
   startTime = time.time()
-  AllSorts.bubbleSort(orderedList)
+  AllSorts.mergeSort(orderedList)
   endTime = time.time()
   elapsedTime = endTime - startTime
   print("Ordered list time: %.5f seconds" % elapsedTime)
 
   startTime = time.time()
-  AllSorts.bubbleSort(reversedList)
+  AllSorts.mergeSort(reversedList)
   endTime = time.time()
   elapsedTime = endTime - startTime
   print("Reversed list time: %.5f seconds" % elapsedTime)
 
   startTime = time.time()
-  AllSorts.bubbleSort(randomList)
+  AllSorts.mergeSort(randomList)
   endTime = time.time()
   elapsedTime = endTime - startTime
   print("Random list time: %.5f seconds" % elapsedTime)
